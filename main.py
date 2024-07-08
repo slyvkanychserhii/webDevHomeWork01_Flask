c2c9153e7a58c52174a325ac7500e7f2a13ba340
slyvkanychserhii <slyvkanychserhii@gmail.com>
2024-07-08 23:02:19 +0200

    Second commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello, Flask!'
+
+
+# @app.route('/about')
+# def about():
+#     return 'This is the about page.'
+
+
+@app.route('/user/<username>')
+def hello(username):
+    return f'Hello, {username.capitalize()}!'
+
+
+# @app.route('/sum/<int:arg1>/<int:arg2>')
+# def _sum(arg1, arg2):
+#     return f'{arg1} + {arg2} = {arg1 + arg2}'
+
+
+# @app.route('/files/<path:filepath>')
+# def show_file(filepath):
+#     return f'File located at: {filepath}'
+
+
+# ОШИБКА - аргумент arg1 съедается агрументом filepath
+# @app.route('/files/<path:filepath>/<int:arg1>')
+# def show_file(filepath, arg1):
+#     return f'File located at: {filepath}'
+
+
+# @app.route('/user/<uuid:user_id>')
+# def show_user_profile_by_uuid(user_id):
+#     print(type(user_id))
+#     return f'User with UUID: {user_id}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
